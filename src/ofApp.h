dbc57aeaa78056faa9fcca2342a4ff926a32cce1
Rebecca Nordhauser <pointeplusplus@gmail.com>
2014-11-02 19:36:56 -0500

    particle system tested and merge error fix

--- src/ofApp.h
+++ src/ofApp.h
@@ -4,11 +4,8 @@
 //#include "ofxOpenCV.h"
 #include "ofxKinect.h"
 
-<<<<<<< HEAD
 #include "particleChain.h"
-=======
 #include "keycode.h"
->>>>>>> e24ae659ce14923d96d772641cf777c7c68f2160
 
 class ofApp : public ofBaseApp{
 
